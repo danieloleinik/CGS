1e7086c05d73e280523ccb848ef3e7a28b8d2e91
danieloleinik <daniel.oleinik@furukawaelectric.com>
2022-04-30 11:00:02 -0300

    comentario de teste na branch

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
             System.out.println("**** MENU ****");
             System.out.println("(1) Cadastrar Usuário");
-            System.out.println("(2) Listar Usuario");
+            System.out.println("(2) Listar Usuario"); //teste do Daniel na branch listar_usuario_novo
             System.out.println("(3) Deletar Usuário");
             System.out.println("(4) Atualizar Usuário\n");
             System.out.println("(9) Sair\n");
